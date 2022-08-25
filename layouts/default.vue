c8f382ff689c42e68986f725eb163127c4a7f52f
Jean Agostini <agostinijean@hotmail.com>
2022-08-24 21:23:23 -0300

    Aba como comprar e navbar atualizada

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="mainLayout">
     <v-app-bar color="#F7E9D7">
-      <v-app-bar-title>Kame-Kunhru</v-app-bar-title>
+      <v-btn text nuxt to="/" depressed>Kame-Kunhru</v-btn>
       <v-spacer></v-spacer>
-      <v-btn text>Quem somos</v-btn>
+      <v-btn text nuxt to="/como-comprar">Quem somos</v-btn>
       <v-btn text>Como comprar</v-btn>
     </v-app-bar>
     <Nuxt />
@@ -18,6 +18,6 @@ export default {}
 .mainLayout{
   background-color: #EBD8C3;
   min-height: 100vh;
-  height: 100%;
+  height: 100vh + 5vh;
 }
 </style>
