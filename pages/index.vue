7c32e1c96f28eb9bfd5a9df473dd0c87d96b240c
Jean Agostini <agostinijean@hotmail.com>
2022-08-24 21:59:35 -0300

    Inicio da construção da grid de produtos

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    
+    <v-container class="container">
+      <v-row no-gutters>
+        <v-col v-for="n in 3" :key="n">
+          <v-card class="cardProduto">{{n}}</v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -15,4 +21,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.mainLayout > div:nth-child(2){
+  margin: 2%;
+}
+
+.cardProduto{
+  margin: 2%;
+  height: 20vh;
+}
 </style>
